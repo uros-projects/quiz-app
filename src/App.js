5dcf7d54f3023f9a472e876be39e2d5caf15b2de
Uros <uros.tomasevic.work@gmail.com>
2021-01-16 22:47:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+
 import './App.css';
+import QuizData from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [questions, setQuestions] = useState([])
+	const [score, setScore] = useState(0)
+	const [responseCount, setResponseCount] = useState(0)
+
+	const getQuestions = () => {
+		const data = () => QuizData.sort(() => 0.5 - Math.random()).slice(0, 5);
+		setQuestions(data())
+	}
+
+	useEffect( () => {
+		getQuestions()	
+	}, [])
+
+	const playAgain = () => {
+		getQuestions()
+		setScore(0)
+		setResponseCount(0)
+	}
+
+	const computeAnswer = (answer, correctAnswer) => {
+		if(answer === correctAnswer) {
+			setScore( prevScore => prevScore + 1 )
+		}
+		
+		setResponseCount( prevResponseCount => prevResponseCount < 5 ? prevResponseCount + 1 : 5 )
+	}
+
+	return (
+		<main className='container'>
+			<div className='title'>Quiz App</div>
+
+			<div>
+				{questions.length > 0 &&
+					responseCount < 5 &&
+						questions.map( ({question, answers, correct, questionId}) => (
+							<QuestionBox
+								key={questionId} 
+								question={question} 
+								options={answers}
+								selected={ answer => computeAnswer(answer, correct) }
+							/>
+						))
+				}
+
+				{responseCount === 5 ? (
+					<Result score={score} playAgain={playAgain} />
+				) : (
+					null
+				)
+				}
+			</div>
+		</main>
+	);
 }
 
-export default App;
+export default App
